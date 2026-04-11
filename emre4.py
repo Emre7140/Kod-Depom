002a764b2b6208e284884f258c8d393fa79c08a2
Emre7140 <kivancyusuf71@gmail.com>
2026-04-11 20:08:11 +0300

    Create emre4.py

---
+++ emre4.py
@@ -0,0 +1,6 @@
+a=input("Kullanıcı Adınızı Giriniz: ")
+b=input("Şifrenizi Giriniz: ")
+if a=="Türkiye" and b=="1923":
+    print("Giriş Başarılı")
+else:
+    print("Kullanıcı Adı ya da Şifre Yanlış")
