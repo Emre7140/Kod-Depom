33eeb9907eda04de2534ab3e4fe6d63c141e38a2
Emre7140 <kivancyusuf71@gmail.com>
2026-04-11 20:39:18 +0300

    Create emre11.py

---
+++ emre11.py
@@ -0,0 +1,9 @@
+plaka=input("Plaka Giriniz?(06/07/71)")
+if plaka=="06":
+    print("Ankara")
+elif plaka=="07":
+    print("Antalya")
+elif plaka=="71":
+    print("Kırıkkale")
+else:
+    print("TÜRKİYE")
