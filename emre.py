c9eb20d6fb44ba991a7f399454e7f4ff77c0a215
Emre7140 <kivancyusuf71@gmail.com>
2026-04-11 19:57:18 +0300

    İlk kodumu ekledim

---
+++ emre.py
@@ -0,0 +1,8 @@
+a=int(input("1. açınızı giriniz: "))
+b=int(input("2. açınızı giriniz: "))
+c=int(input("3. açınızı giriniz: "))
+d=a+b+c
+if d==180:
+    print("Bu bir üçgendir")
+else:
+    print("Bu bir üçgen değildir")
